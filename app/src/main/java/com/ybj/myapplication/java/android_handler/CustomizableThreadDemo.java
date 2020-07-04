65ef6503587912e46b5313c080bf3e99bb8cf84f
YBJ <15390264297@163.com>
2020-07-04 19:20:58 +0800

    Handler及HandlerThread

---
+++ app/src/main/java/com/ybj/myapplication/java/android_handler/CustomizableThreadDemo.java
@@ -0,0 +1,7 @@
+package com.ybj.myapplication.java.android_handler;
+
+/**
+ * Created by 杨阳洋 on 2020/6/28.
+ */
+public class CustomizableThreadDemo {
+}
---
+++ app/src/main/java/com/ybj/myapplication/kotlin/handler/HandlerThreadActivity.kt
@@ -0,0 +1,12 @@
+package com.ybj.myapplication.kotlin.handler
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HandlerThreadActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_handler_thread)
+    }
+}
