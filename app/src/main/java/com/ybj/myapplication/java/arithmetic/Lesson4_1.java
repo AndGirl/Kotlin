d8d752a6f49227857b3cc049e019f0c7523886d1
YBJ <15390264297@163.com>
2020-07-11 02:26:46 +0800

    链表尝试

---
+++ app/src/main/java/com/ybj/myapplication/java/arithmetic/Lesson4_1.java
@@ -0,0 +1,7 @@
+package com.ybj.myapplication.java.arithmetic;
+
+/**
+ * Created by 杨阳洋 on 2020/7/10.
+ */
+public class Lesson4_1 {
+}
---
+++ app/src/main/java/com/ybj/myapplication/java/arithmetic/tools/SingLinkedList.java
@@ -0,0 +1,20 @@
+package com.ybj.myapplication.java.arithmetic.tools;
+
+/**
+ * Created by 杨阳洋 on 2020/7/10.
+ * 链表
+ */
+public class ListNode {
+    public static class Node{
+        //指针
+        Node next = null;
+        int data;
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node head = null;
+    
+
+}
