318e60ad57ebf1ff2e4175681bb0401b000b634f
YBJ <15390264297@163.com>
2020-07-13 23:24:44 +0800

    RecyelrView-ItemDecoration

---
+++ app/src/main/java/com/ybj/myapplication/kotlin/recyclerView/ItemDecorationRecyclerViewActivity.kt
@@ -0,0 +1,12 @@
+package com.ybj.myapplication.kotlin.recyclerView
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemDecorationRecyclerViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_decoration_recycler_view)
+    }
+}
