a69a09e92134364cacab0176f53296dd363bcd61
YBJ <15390264297@163.com>
2020-07-16 00:56:51 +0800

    简单的自定义View

---
+++ app/src/main/java/com/ybj/myapplication/kotlin/okhttp/OkHttpActivity.kt
@@ -0,0 +1,12 @@
+package com.ybj.myapplication.kotlin.okhttp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OkHttpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ok_http)
+    }
+}
---
+++ app/src/main/java/com/ybj/myapplication/kotlin/view/CircleAvatarActivity.kt
@@ -0,0 +1,12 @@
+package com.ybj.myapplication.kotlin.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CircleAvatarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_circle_avatar)
+    }
+}
