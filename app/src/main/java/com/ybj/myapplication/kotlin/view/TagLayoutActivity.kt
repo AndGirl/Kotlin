a3bd78abe8d87bcfbb9824d08cb953a008c5df31
YangBinJie <15390264297@163.com>
2020-07-26 12:47:27 +0800

    1

---
+++ app/src/main/java/com/ybj/myapplication/kotlin/view/TagLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.ybj.myapplication.kotlin.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TagLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tag_layout)
+    }
+}
