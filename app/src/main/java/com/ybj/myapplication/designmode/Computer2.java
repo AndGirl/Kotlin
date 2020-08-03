f410ab5235353e93d0647290a059bd4ad27ead4e
YangBinJie <15390264297@163.com>
2020-08-03 15:25:01 +0800

    设计模式初体验

---
+++ app/src/main/java/com/ybj/myapplication/designmode/Computer2.java
@@ -0,0 +1,11 @@
+package com.ybj.myapplication.designmode;
+
+/**
+ * Created by 杨阳洋 on 2020/8/3.
+ * 抽象产品类：需要创建所有对象的抽象父类，负责描述所有实例共有的公共接口
+ */
+public abstract class Computer {
+
+    public abstract void start();
+
+}
---
+++ app/src/main/java/com/ybj/myapplication/designmode/Swordsman.java
@@ -0,0 +1,7 @@
+package com.ybj.myapplication.designmode;
+
+/**
+ * Created by 杨阳洋 on 2020/8/3.
+ */
+public class Swordsman {
+}
